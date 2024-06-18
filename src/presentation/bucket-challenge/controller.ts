8dd44952ce2d1bd6a279fd2a5a9ebcf584b6f904
FranciscoJSB12 <javif8@gmail.com>
2024-06-18 19:02:54 -0400

    feat: Add bucket challenge controller

--- src/presentation/bucket-challenge/controller.ts
+++ src/presentation/bucket-challenge/controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
+import { BucketChallengeService } from '../services/bucket-challenge.service';
 
 export class BucketChallengeController {
+
+  constructor(
+    private readonly bucketChallengeService = new BucketChallengeService(),
+  ){}
+
   public getBucketChallengeData = ( req: Request, res: Response ) => {
-    res.json({
-        message: "Server is working properly"
-    });
+    res.json(this.bucketChallengeService.executeCalculation());
   };
 } 
---
+++ src/presentation/services/bucket-challenge.service.ts
@@ -0,0 +1,5 @@
+export class BucketChallengeService {
+  public executeCalculation() {  
+    return { status: 'ok', data: 'this is the place for the data and calculation (TODO)' }
+  }
+}
