3aa14195ebf6bd3f8e32ccadd822deaa4d274229
FranciscoJSB12 <javif8@gmail.com>
2024-06-20 18:02:42 -0400

    test: Set up testing environment

---
+++ tests/presentation/helpers/bucket-challenge-best-solution/dataReceiver.helper.test.ts
@@ -0,0 +1,9 @@
+import { DataReceiver } from "../../../../src/presentation/helpers/bucket-challenge-best-solution/dataReceiver.helper";
+
+describe('Data-Receiver-helper-test', () => {
+  test('should be true', () => {
+
+    expect(true).toBe(true);
+
+  });
+});
