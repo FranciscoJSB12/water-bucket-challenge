4d9e8d76d40815dcb6b2508d388989b46b41c76a
FranciscoJSB12 <javif8@gmail.com>
2024-06-20 18:46:40 -0400

    test: Add test cases to check proper behavior

--- tests/presentation/helpers/bucket-challenge-best-solution/dataReceiver.helper.test.ts
+++ tests/presentation/helpers/bucket-challenge-best-solution/dataReceiver.helper.test.ts
@@ -1,9 +1,60 @@
 import { DataReceiver } from "../../../../src/presentation/helpers/bucket-challenge-best-solution/dataReceiver.helper";
+import { firstCase, secondCase } from "../../../../testdata/expectedResult";
 
-describe('Data-Receiver-helper-test', () => {
-  test('should be true', () => {
+describe('Data-Receiver-helper', () => {
+  test('First case with BucketX=2 BucketY=10 AmountWanted=4', () => {
 
-    expect(true).toBe(true);
+    //1. Arrange 
+    const bucketX = 2;
+    const bucketY = 10;
+    const amountWantedZ = 4;
 
+    //2. Act 
+    const dataReceiver = new DataReceiver();
+
+    const result = dataReceiver.obtainData({ bucketX, bucketY, amountWantedZ });
+
+    //3. Assert
+    expect(result).toEqual({
+      ok: true,
+      isSolutionPossible: true,
+      results: firstCase,
+    });
+  });
+
+
+  test('Second case with BucketX=2 BucketY=100 AmountWanted=96', () => {
+
+    const bucketX = 2;
+    const bucketY = 100;
+    const amountWantedZ = 96;
+
+    
+    const dataReceiver = new DataReceiver();
+
+    const result = dataReceiver.obtainData({ bucketX, bucketY, amountWantedZ });
+  
+    expect(result).toEqual({
+      ok: true,
+      isSolutionPossible: true,
+      results: secondCase,
+    });
+  });
+
+  test('Third case with BucketX=2 BucketY=6 AmountWanted=5', () => {
+
+    const bucketX = 2;
+    const bucketY = 6;
+    const amountWantedZ = 5;
+
+    const dataReceiver = new DataReceiver();
+
+    const result = dataReceiver.obtainData({ bucketX, bucketY, amountWantedZ });
+
+    expect(result).toEqual({
+      ok: true,
+      isSolutionPossible: false,
+      results: [],
+    });
   });
 });
